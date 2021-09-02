fe603306c7450bc91a7d81d099e6eff1906bd20a
Santo Commarato <cylynex@gmail.com>
2021-09-02 11:51:38 -0600

    Switched from Text Input to Buttons

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -22,50 +22,32 @@ public class GameController : MonoBehaviour {
         InitializeSector();
     }
 
-    private void Update() {
-        GetInput(); 
-    }
+    void InitializeSector() {
 
-    void GetInput() {
-
-        for (int index = 0;index < sectorData.connectedSectors.Length;index++) {
-            if (Input.GetKeyDown(KeyCode.Alpha1 + index)) {
-                currentSector = sectorData.connectedSectors[index];
-                InitializeSector();
-            }
+        // Clear anything in the action box (currently just using sector connection box)
+        foreach (Transform child in sectorConnectionsBox) {
+            Destroy(child.gameObject);
         }
 
-    }
-
-    void InitializeSector() {
         sectorData = currentSector.GetSectorData();
-        SetUIText(sectorFlavorText, sectorData.sectorFlavorText);
-        SetUIText(sectorName, sectorData.sectorName);
         ShowConnections();
     }
 
     void ShowConnections() {
-        //int keycode = 1;
-        //string output = "";
         for (int i = 0;i < sectorData.connectedSectors.Length;i++) {
-            //output += keycode + " - " + sectorData.connectedSectors[i].sectorName+"("+i+")\n";
-            //keycode++;
-
             GameObject thisButton = Instantiate(changeSectorButton, transform.position, transform.rotation);
             thisButton.transform.parent = sectorConnectionsBox;
-            thisButton.GetComponentInChildren<Text>().text = sectorData.connectedSectors[i].sectorName;
+            thisButton.GetComponentInChildren<Text>().text = "M: "+sectorData.connectedSectors[i].sectorName;
             Sector buttonSector = sectorData.connectedSectors[i];
             Button button = thisButton.GetComponent<Button>();
             button.onClick.AddListener(() => ChangeSector(buttonSector));
         }
-        
-        //SetUIText(sectorConnections, output);
     }
 
     void ChangeSector(Sector newSector) {
-        print(newSector.sectorName);
+        currentSector = newSector;
+        InitializeSector();
     }
-
 
     void SetUIText(Text field, string textToSet) {
         field.text = textToSet;
